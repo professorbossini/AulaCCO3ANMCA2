619446b59f07ce9911f6854a0114046f78db344b
rodrigo <rod.bossini@gmail.com>
2017-09-05 22:40:19 -0300

    Tudo ok, agora com torrada. Até a Claudia conseguiu ver.

--- app/src/main/java/br/com/bossini/meuprimeiroapp/MainActivity.java
+++ app/src/main/java/br/com/bossini/meuprimeiroapp/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.bossini.meuprimeiroapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Exibindo uma mensagem numa torrada", Toast.LENGTH_SHORT).show();
 
     }
 }
