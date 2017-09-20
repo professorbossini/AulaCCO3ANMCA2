afde5b33f2d83e69c290a352369aa7bc80f9045e
rodrigo <rod.bossini@gmail.com>
2017-09-19 22:36:34 -0300

    Agora completamos o app. Todos os direitos reservados.

---
+++ app/src/main/java/br/com/bossini/meuprimeiroapp/DisplayMessageActivity.java
@@ -0,0 +1,24 @@
+package br.com.bossini.meuprimeiroapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent i = getIntent();
+        String t = i.getStringExtra(MainActivity.msg);
+        TextView textView = new TextView (this);
+        textView.setTextSize(40);
+        textView.setText(t);
+        ViewGroup layout = (ViewGroup)
+                findViewById(R.id.activity_display_message);
+        layout.addView(textView);
+    }
+}
--- app/src/main/java/br/com/bossini/meuprimeiroapp/MainActivity.java
+++ app/src/main/java/br/com/bossini/meuprimeiroapp/MainActivity.java
@@ -1,16 +1,30 @@
 package br.com.bossini.meuprimeiroapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //inflando a view
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Exibindo uma mensagem numa torrada", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Exibindo uma mensagem numa torrada", Toast.LENGTH_SHORT).show();
 
     }
+
+    public void sendMessage (View view){
+        //Toast.makeText(this, ((Button) view).getText(), Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String s = editText.getEditableText().toString();
+        i.putExtra("msg", s);
+        startActivity(i);
+    }
+
+    public static final String msg = "msg";
 }
